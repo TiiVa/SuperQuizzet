78029e1f4bdea4b5b5ae6ca2d144df46ca80e9b9
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-08 14:10:03 +0100

    update

--- src/javaScript/quiz.js
+++ src/javaScript/quiz.js
@@ -5,11 +5,132 @@ class Question {
   }
 }
 
+const json = `
+{
+    "response_code": 0,
+    "results": [
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "The logo for Snapchat is a Bell.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "The Windows 7 operating system has six main editions.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "RAM stands for Random Access Memory.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "Linux was first created as an alternative to Windows XP.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "The Windows ME operating system was released in the year 2000.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "Linus Torvalds created Linux and Git.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "Science: Computers",
+            "question": "Time on Computers is measured via the EPOX System.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        }
+    ]
+}
+`;
+
+const response = JSON.parse(json);
+
+const q2 = response["results"];
+
 const questions = [
   new Question("Hej", true),
   new Question("Då", false),
   new Question("Är CSS kul?", false),
 ];
+
+for (const fråga of q2) {
+  let correctAnswer = fråga["correct_answer"] === "True";
+
+  const q = new Question(fråga["question"], correctAnswer);
+  questions.push(q);
+}
+
+console.log(q2);
 
 console.log(questions);
 
@@ -24,8 +145,6 @@ displayQuestions();
 
 function displayQuestions() {
   for (const question of questions) {
-    console.log(question);
-
     const li = document.createElement("li");
     const card = document.createElement("div");
     const cardBody = document.createElement("div");
