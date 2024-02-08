f592f09e73ac1e5bdb17ab697ea42867a22c170f
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-08 11:37:26 +0100

    update

--- src/javaScript/quiz.js
+++ src/javaScript/quiz.js
@@ -10,6 +10,9 @@ const questions = [new Question("Hej", true), new Question("Då", false)];
 console.log(questions);
 
 const questionsList = document.querySelector("#questions");
+const score = document.querySelector("#score");
+
+let scoreCount = 0;
 
 console.log(questionsList);
 
@@ -27,13 +30,37 @@ for (const question of questions) {
   li.classList.add("list-group-item", "bg-dark", "border-danger");
   card.classList.add("card", "bg-dark", "container");
   cardBody.classList.add("card-body", "row");
-  cardFooter.classList.add("card-footer", "row", "container");
+  cardFooter.classList.add(
+    "card-footer",
+    "row",
+    "container",
+    "justify-content-center"
+  );
   cardTitle.classList.add("card-title", "text-info", "text-center");
-  trueBtn.classList.add("btn", "btn-success", "mx-1", "col");
-  falseBtn.classList.add("btn", "btn-danger", "mx-1", "col");
+  trueBtn.classList.add("btn", "btn-success", "mx-1", "col-2");
+  falseBtn.classList.add("btn", "btn-danger", "mx-1", "col-2");
 
   trueBtn.innerText = "True";
+  trueBtn.onclick = () => {
+    if (question.correctAnswer === true) {
+      console.log(`Tjohooo! Du gissade rätt`);
+      scoreCount++;
+      score.innerText = scoreCount;
+    } else {
+      console.log("HAHA FEL!");
+    }
+  };
+
   falseBtn.innerText = "False";
+  falseBtn.onclick = () => {
+    if (question.correctAnswer === false) {
+      console.log(`Tjohooo! Du gissade rätt`);
+      scoreCount++;
+      score.innerText = scoreCount;
+    } else {
+      console.log("HAHA FEL!");
+    }
+  };
 
   cardTitle.innerText = question.statement;
 
